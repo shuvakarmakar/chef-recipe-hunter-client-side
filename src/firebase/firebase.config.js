4f6e256897253b393d28eb4e1943cd913eacff4a
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-05-02 02:02:27 +0600

    .env local added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDtB5FK8Mu_haKk1bzfmSj_svcNWzdPiD4",
-  authDomain: "chef-recipe-hunter-409b5.firebaseapp.com",
-  projectId: "chef-recipe-hunter-409b5",
-  storageBucket: "chef-recipe-hunter-409b5.appspot.com",
-  messagingSenderId: "698714161265",
-  appId: "1:698714161265:web:2dcff0345fa431137d1bd0"
+    apiKey: import.meta.env.VITE_apiKey,
+    authDomain: import.meta.env.VITE_authDomain,
+    projectId: import.meta.env.VITE_projectId,
+    storageBucket: import.meta.env.VITE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_messagingSenderId,
+    appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
